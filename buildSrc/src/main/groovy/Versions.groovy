9e5fb3a6821c57248e7de44f7773dafd23573a4f
杨耀飞 <yangyaofei@gmail.com>
2023-11-02 17:30:47 +0800

    version v0.2.3.1
    1. unify version with Versions.groovy
    2. add spring dependency instead of pom
    3. add version resolve for maven publish

---
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -0,0 +1,37 @@
+class Versions {
+    // NLPIR versions
+    static final String nlpirPlatformUtilsVersion = "0.2.3.1bbb"
+    static final String nlpirClientJavaVersion = "0.5.8.1-beta"
+    static final String nlpirDocumentUtils = "0.4.9-beta"
+    static final String nlpirAuthorizationVServerVersion = "0.1.5"
+
+    // apache commons
+    static final String commonsLangVersion = "3.12.0" // for non-spring project
+    static final String commonsCodecVersion = "1.15"
+    static final String commonsIoVersion = "2.11.0"
+    static final String commonsMath3Version = "3.6.1"
+    static final String commonsTextVersion = "1.11.0"
+    static final String commonsCollections4Version = "4.4"
+
+    // grpc
+    static final String grpcVersion = "1.55.1"
+    static final String protobufVersion = "3.22.2"
+
+    // other versions
+    static final String minioVersion = "8.5.2"
+    static final String dockerJavaVersion = "3.3.0"
+    static final String testContainerVersion = '1.18.3'
+    static final String postgresqlVersion = "42.5.4"
+    static final String javaJwtVersion = "4.4.0"
+    static final String googleJsr305 = "3.0.2"
+
+    static final String googleGuava = "32.1.2-jre"
+
+    static final String jakartaValidationApiVersion = "3.0.2"
+
+    static final String modelMapperVersion = "3.1.1"
+    static final String modelMapperModuleRecordVersion = "1.0.0"
+
+    static final String bouncycastleVersion = "bcpkix-jdk18on:1.76"
+
+}
